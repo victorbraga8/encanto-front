39ed090623edc76316cde7905176712edb3d2c0e
Victor Braga <victorbraga.8@hotmail.com>
2024-02-29 20:51:47 -0300

    Commit de Backup Sidebar Menu

--- app/components/sidebar/page.tsx
+++ app/components/sidebar/page.tsx
@@ -1,31 +1,21 @@
-"use client";
 import {
   ChevronDown,
-  ChevronFirst,
   ChevronLeft,
   Cog,
-  MoreVertical,
   ShoppingBag,
   User,
   Users,
 } from "lucide-react";
-import { useRouter } from "next/router";
 import { useState } from "react";
 
 const Sidebar = () => {
-  //   const router = useRouter();
   const [collapsed, setCollapsed] = useState(false);
+  const [adminCollapsed, setAdminCollapsed] = useState(false);
+  const [programasCollapsed, setProgramasCollapsed] = useState(true);
 
   const toggleCollapse = () => {
     setCollapsed(!collapsed);
     setAdminCollapsed(false);
-  };
-
-  const [adminCollapsed, setAdminCollapsed] = useState(false);
-  const [programasCollapsed, setProgramasCollapsed] = useState(true);
-
-  const toggleCollapsed = () => {
-    setCollapsed((prevCollapsed) => !prevCollapsed);
   };
 
   const toggleAdminCollapsed = () => {
@@ -71,25 +61,53 @@ const Sidebar = () => {
             <span className="mr-2">
               {collapsed ? <Cog size={20} /> : "Admin"}
             </span>
-            {!collapsed && adminCollapsed && (
-              <ul className="pl-4">
-                <li className="py-2 px-3 text-center hover:bg-gray-200 cursor-pointer flex items-center">
-                  <Users size={16} className="mr-2" />
-                  Programas
-                </li>
-                <li className="py-2  px-3 text-center hover:bg-gray-200 cursor-pointer flex items-center">
-                  <ShoppingBag size={16} className="mr-2" />
-                  Shopping
-                </li>
-              </ul>
-            )}
           </li>
-          <li className="p-4 hover:bg-gray-700 cursor-pointer">
+          {!collapsed && adminCollapsed && (
+            <ul className="pl-4">
+              <li
+                className={`py-2 px-3 text-center ${
+                  programasCollapsed ? "bg-cyan-700" : "hover:bg-gray-700"
+                } cursor-pointer flex items-center`}
+                onClick={toggleProgramasCollapsed}
+              >
+                <Users size={16} className="mr-2" />
+                Programas
+              </li>
+              {!programasCollapsed && (
+                <ul className="pl-4">
+                  <li className="py-2 px-3 text-center hover:bg-gray-200 cursor-pointer flex items-center">
+                    Cadastrar
+                  </li>
+                  <li className="py-2 px-3 text-center hover:bg-gray-200 cursor-pointer flex items-center">
+                    Listar
+                  </li>
+                </ul>
+              )}
+              <li
+                className={`py-2  px-3 text-center ${
+                  collapsed ? "hover:bg-gray-200" : "bg-gray-700"
+                } cursor-pointer flex items-center`}
+              >
+                <ShoppingBag size={16} className="mr-2" />
+                Shopping
+              </li>
+            </ul>
+          )}
+
+          <li
+            className={`p-4 ${
+              collapsed ? "hover:bg-gray-700" : "bg-gray-700"
+            } cursor-pointer`}
+          >
             <span className="mr-2">
               {collapsed ? <User size={20} /> : "Users"}
             </span>
           </li>
-          <li className="p-4 hover:bg-gray-700 cursor-pointer">
+          <li
+            className={`p-4 ${
+              collapsed ? "hover:bg-gray-700" : "bg-gray-700"
+            } cursor-pointer`}
+          >
             <span className="mr-2">
               {collapsed ? <Cog size={20} /> : "Config"}
             </span>
