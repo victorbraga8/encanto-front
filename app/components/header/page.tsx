e6c5372faaec23275dd5c67c1c9fc4d0dc6deb90
Victor Braga <victorbraga.8@hotmail.com>
2024-03-04 10:55:58 -0300

    Correção Componente Header

--- app/components/header/page.tsx
+++ app/components/header/page.tsx
@@ -14,7 +14,7 @@ import helpers from "@/lib/helpers";
 // import sessionData from "@/lib/helpers";
 
 const Header = () => {
-  //   const { data } = useSession();
+  const { data } = useSession();
   const router = useRouter();
   const pathName = usePathname();
 
@@ -31,38 +31,37 @@ const Header = () => {
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-end p-12 bg-gradient-to-r from-cyan-400 to-indigo-700 py-4 gap-3">
-        {/* {data?.user?.name ? ( */}
         <>
-          <div className="flex-grow">
-            <h2 className="ml-[220px] text-white">
-              <p className="uppercase">
-                <span className="font-semibold">{pathParts}</span>
-              </p>
-            </h2>
-          </div>
-          <div className="mr-0 cursor-pointer">
-            <MessageCircle className="text-white" />
-          </div>
-          <div className="mr-4 cursor-pointer">
-            <Settings className="text-white" />
-          </div>
-          <h2 className="text-white font-normal uppercase text-xs">
-            {/* {data.user?.name} */}
-          </h2>
-          <Avatar className="h-10 w-10 ">
-            <AvatarImage
-              src="/assets/profile-pic-new.jpg"
-              className="rounded-full border-2 border-solid border-white"
-            />
-          </Avatar>
+          {data?.user ? (
+            <>
+              <div className="flex-grow">
+                <h2 className="ml-[220px] text-white">
+                  <p className="uppercase">
+                    <span className="font-semibold">{pathParts}</span>
+                  </p>
+                </h2>
+              </div>
+
+              <div className="mr-0 cursor-pointer">
+                <MessageCircle className="text-white" />
+              </div>
+              <div className="mr-4 cursor-pointer">
+                <Settings className="text-white" />
+              </div>
+              <h2 className="text-white font-normal uppercase text-xs">
+                {data?.user?.name}
+              </h2>
+              <Avatar className="h-10 w-10 ">
+                <AvatarImage
+                  src="/assets/profile-pic-new.jpg"
+                  className="rounded-full border-2 border-solid border-white"
+                />
+              </Avatar>
+            </>
+          ) : (
+            <div className="py-5"></div>
+          )}
         </>
-        {/* ) : (
-          <div className="h-10 w-10 ">
-            <h2 className="text-white font-normal uppercase text-xs">
-              <Button onClick={() => signIn("azure-ad")}>Login</Button>
-            </h2>
-          </div>
-        )} */}
       </CardContent>
     </Card>
   );
