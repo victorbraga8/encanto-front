b7b8f90a8a63d475f26c0fff9f7f1283b11dbc42
Victor Braga <victorbraga.8@hotmail.com>
2024-03-11 16:07:36 -0300

    Correção Dados AD

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,11 +4,11 @@ import AzureADProvider from "next-auth/providers/azure-ad";
 const handler = NextAuth({
   providers: [
     AzureADProvider({
-      clientId: "2965b143-82d0-4c53-9667-263b0fc5a202",
-      clientSecret: "ubo8Q~SSmDT7IEEYoQ0l7cXg9~H1uC0k~BcsFbYN",
+      clientId: "cb53228b-5ac1-4c44-bea1-cc7d73b82d6c",
+      clientSecret: ".4R8Q~x1O5JbR.EBtEP_E.MV1lN2GdgX-QhIVdhh",
       // clientId: "c69c5892-8501-4622-955f-2cb696dca018",
       // clientSecret: "FPV8Q~izBHxLzhGx7iQszV046J1c3Vw~Cwi4nb2i",
-      tenantId: "7ef724cb-3c77-4b44-90bd-1a48e265ad86",
+      tenantId: "e21e0d76-cf85-4728-a5d8-c94ec48156ed",
     }),
   ],
 });
