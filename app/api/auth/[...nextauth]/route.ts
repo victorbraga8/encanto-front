16925214d68d648879836a6fc04ac707a9fcf6a4
Victor Braga <victorbraga.8@hotmail.com>
2024-03-11 19:48:29 -0300

    Atualização route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,13 +4,12 @@ import AzureADProvider from "next-auth/providers/azure-ad";
 const handler = NextAuth({
   providers: [
     AzureADProvider({
-      clientId: "cb53228b-5ac1-4c44-bea1-cc7d73b82d6c",
-      clientSecret: ".4R8Q~x1O5JbR.EBtEP_E.MV1lN2GdgX-QhIVdhh",
-      // clientId: "c69c5892-8501-4622-955f-2cb696dca018",
-      // clientSecret: "FPV8Q~izBHxLzhGx7iQszV046J1c3Vw~Cwi4nb2i",
-      tenantId: "e21e0d76-cf85-4728-a5d8-c94ec48156ed",
+      clientId: process.env.AZURE_AD_CLIENT_ID!!!,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET!!!,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
     }),
   ],
+  secret: process.env.NEXT_AUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
