b9115a64f8dc373a79b391ef507a72e4b4abba1f
Victor Braga <victorbraga.8@hotmail.com>
2024-03-12 17:40:23 -0300

    Atualização AD Vercel

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,23 @@
 import NextAuth from "next-auth";
 import AzureADProvider from "next-auth/providers/azure-ad";
 
+const clientIdAd = process.env.AZURE_AD_CLIENT_ID;
+const clientSecret = process.env.AZURE_AD_CLIENT_SECRET;
+const tenantId = process.env.AZURE_AD_TENANT_ID;
+
+if (!clientIdAd || !clientSecret || !tenantId) {
+  throw new Error("Azure AD client ID not provided.");
+}
+
 const handler = NextAuth({
   providers: [
     AzureADProvider({
-      clientId: process.env.AZURE_AD_CLIENT_ID!!!,
-      clientSecret: process.env.AZURE_AD_CLIENT_SECRET!!!,
-      tenantId: process.env.AZURE_AD_TENANT_ID,
+      clientId: clientIdAd,
+      clientSecret: clientSecret,
+      tenantId: tenantId,
     }),
   ],
-  secret: process.env.NEXT_AUTH_SECRET,
+  secret: "secret",
 });
 
 export { handler as GET, handler as POST };
