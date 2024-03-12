767fb4219ea5dcb7d3ce6f05f5e8bfebf44435c8
Victor Braga <victorbraga.8@hotmail.com>
2024-03-12 17:47:14 -0300

    Troca de Credenciais AD para ambiente Encanto

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    AZURE_AD_CLIENT_ID: "2965b143-82d0-4c53-9667-263b0fc5a202",
-    AZURE_AD_CLIENT_SECRET: "JQm8Q~iQmvWFlhvkYNFbHgZatddS3fuy3DxVRdzh",
-    AZURE_AD_TENANT_ID: "7ef724cb-3c77-4b44-90bd-1a48e265ad86",
+    AZURE_AD_CLIENT_ID: "ddfa597e-e195-489b-80e8-5c4b273e67f6",
+    AZURE_AD_CLIENT_SECRET: "nvd8Q~Pi2P~q8XsxcLrOpgmRFCTJpc71_O7DDamd",
+    AZURE_AD_TENANT_ID: "e21e0d76-cf85-4728-a5d8-c94ec48156ed",
     NEXT_AUTH_SECRET: "secret",
   },
 };
